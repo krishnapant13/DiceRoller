51df5fa560816732aad037e0521c15eaaac1b094
kp978 <krishna.pant@quodeck.com>
2022-02-23 09:35:46 +0530

    personal account commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import java.util.*
+import java.util.* 
 
 class MainActivity : AppCompatActivity() {
 
